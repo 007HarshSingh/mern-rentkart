3f4e1fa757bdd51037d7c69a66fdcf70cfbee2ec
007HarshSingh <harshsingh0380@gmail.com>
2024-02-04 17:06:59 +0530

    home page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 export default function SignUp() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>
-        SignUp
+      <h1 className='text-3xl text-center font-serif my-7'>
+        Sign Up
       </h1>
       <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 font-medium rounded-lg' id='username' />
-        <input type="text" placeholder='email' className='border p-3 rounded-lg font-medium' id='email' />
-        <input type="text" placeholder='password' className='border p-3 rounded-lg font-medium' id='password' />
+        <input type="text" placeholder='username' className='border p-3  rounded-lg' id='username' />
+        <input type="text" placeholder='email' className='border p-3 rounded-lg ' id='email' />
+        <input type="text" placeholder='password' className='border p-3 rounded-lg ' id='password' />
         <button className='bg-slate-700 font-medium text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '>Sign up</button>
       </form>
       <div className='flex gap-2 mt-5'>
